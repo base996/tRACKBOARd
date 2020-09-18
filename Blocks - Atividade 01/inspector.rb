9a86fbd1e84c8f9d50d59f96f08b0c87aff85293
base996 <115064884+base996@users.noreply.github.com>
2020-09-18 12:01:27 +0000

    change logo

--- Blocks - Atividade 01/inspector.rb
+++ Blocks - Atividade 01/inspector.rb
@@ -3,4 +3,9 @@ keys = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 array_data = Array.new
 
 keys.each do |n|
-  file = File.open("bloc
+  file = File.open("bloco_#{n+1}.txt")
+  file_data = file.read
+  array_data.push(file_data)
+  file.close
+
+  if array_data[n].split().
